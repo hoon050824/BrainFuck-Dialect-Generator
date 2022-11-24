545c129dd0376b88dc1b9d09a1b18a30d8fdbd6b
hoon050824 <46698298+hoon050824@users.noreply.github.com>
2022-11-24 09:52:23 +0900

    Renewing
    Make Translator.html act as menu
    Add BF2DI.html, DI2BF.html
    Use BF2DI.html, DI2BF.html in Translator.html
    Rename script.js to Common.js

---
+++ JavaScript/Common.js
@@ -0,0 +1,200 @@
+var bf_di = {
+    '>': '>',
+    '<': '<',
+    '+': '+',
+    '-': '-',
+    '.': '.',
+    ',': ',',
+    '[': '[',
+    ']': ']'
+};
+
+var di_bf = {
+    '>': '>',
+    '<': '<',
+    '+': '+',
+    '-': '-',
+    '.': '.',
+    ',': ',',
+    '[': '[',
+    ']': ']'
+};
+
+function getPair(){
+    var pair = localStorage.getItem("dl");
+    pair = JSON.parse(pair); console.log(pair)
+
+    bf_di = {
+        '>': pair.gt,
+        '<': pair.lt,
+        '+': pair.plus,
+        '-': pair.minus,
+        '.': pair.dot,
+        ',': pair.comma,
+        '[': pair.open,
+        ']': pair.close
+    };
+    
+    /*
+    di_bf = {
+        pair.gt: '>',
+        pair.lt: '<',
+        pair.plus: '+',
+        pair.minus: '-',
+        pair.dot: '.',
+        pair.comma: ',',
+        pair.open: '[',
+        pair.close: ']'
+    };
+
+    di_bf = {
+        bf_di['>']: '>',
+        bf_di['<']: '<',
+        bf_di['+']: '+',
+        bf_di['-']: '-',
+        bf_di['.']: '.',
+        bf_di[',']: ',',
+        bf_di['[']: '[',
+        bf_di[']']: ']'
+    };
+
+    di_bf[gt] = '>';
+    di_bf[lt] = '<';
+    di_bf[plus] = '+';
+    di_bf[minus] = '-';
+    di_bf[dot] = '.';
+    di_bf[comma] = ',';
+    di_bf[open] = '[';
+    di_bf[close] = ']';
+    */
+
+    try{dispDialect()}catch(e){}
+}
+
+function setPair(){
+    var pair = {
+        gt: bf_di['>'],
+        lt: bf_di['<'],
+        plus: bf_di['+'],
+        minus: bf_di['-'],
+        dot: bf_di['.'],
+        comma: bf_di[','],
+        open: bf_di['['],
+        close: bf_di[']'],
+    }; pair = JSON.stringify(pair, null, "\t");
+    localStorage.setItem("dl", pair)
+}
+
+function dispDialect(){
+    var print_gt = document.getElementById('print_gt');
+    var print_lt = document.getElementById('print_lt');
+    var print_plus = document.getElementById('print_plus');
+    var print_minus = document.getElementById('print_minus');
+    var print_dot = document.getElementById('print_dot');
+    var print_comma = document.getElementById('print_comma');
+    var print_open = document.getElementById('print_open');
+    var print_close = document.getElementById('print_close');
+    var ex_open = document.getElementById('ex_open');
+    var ex_close = document.getElementById('ex_close');
+
+    print_gt.innerText = bf_di['>'];
+    print_lt.innerText = bf_di['<'];
+    print_plus.innerText = bf_di['+'];
+    print_minus.innerText = bf_di['-'];
+    print_dot.innerText = bf_di['.'];
+    print_comma.innerText = bf_di[','];
+    print_open.innerText = bf_di['['];
+    print_close.innerText = bf_di[']'];
+    ex_open.innerText = bf_di[']'];
+    ex_close.innerText = bf_di['['];
+
+    console.log(bf_to_di('><+-.,[]'));
+}
+
+function saveDialect(){
+    var export_file = {
+        isDialect: true,
+        gt: bf_di['>'],
+        lt: bf_di['<'],
+        plus: bf_di['+'],
+        minus: bf_di['-'],
+        dot: bf_di['.'],
+        comma: bf_di[','],
+        open: bf_di['['],
+        close: bf_di[']'],
+    }; export_file = JSON.stringify(export_file, null, "\t");
+
+    var dataURI = "data:application/json; charset=UTF-8," + encodeURIComponent(export_file);
+    var link = document.getElementById('export');
+    link.href = dataURI;
+}
+
+function di_to_bf(code){
+    var s = '';
+
+    for(var i = 0; i <= code.length; i++){
+        tmp = code[i];
+
+        for(var j = i+1; j <= code.length; j++){
+            if(bf_di['>'] == tmp){
+                s += '>';
+                i = j-1;
+                break;
+            } else if(bf_di['<'] == tmp){
+                s += '<';
+                i = j-1;
+                break;
+            } else if(bf_di['+'] == tmp){
+                s += '+';
+                i = j-1;
+                break;
+            } else if(bf_di['-'] == tmp){
+                s += '-';
+                i = j-1;
+                break;
+            } else if(bf_di['.'] == tmp){
+                s += '.';
+                i = j-1;
+                break;
+            } else if(bf_di[','] == tmp){
+                s += ',';
+                i = j-1;
+                break;
+            } else if(bf_di['['] == tmp){
+                s += '[';
+                i = j-1;
+                break;
+            } else if(bf_di[']'] == tmp){
+                s += ']';
+                i = j-1;
+                break;
+            } else {
+                tmp += code[j];
+            }
+            
+            /*
+            if(di_bf[tmp] != undefined){
+                s += di_bf[tmp];
+                i = j-1;
+                break;
+            } else {
+                tmp += code[j];
+            }
+            */
+        }
+    }
+
+    return s;
+}
+
+function bf_to_di(code){
+    var s = '';
+
+    for(var i of code){
+        if(bf_di[i] != undefined){
+            s += bf_di[i];
+        }
+    }
+
+    return s;
+}
--- JavaScript/Translator.js
+++ JavaScript/Translator.js
@@ -1,6 +1,13 @@
-function translate(){
+function BF2DI(){
     var s = document.getElementById("beTranslate");
     var text = bf_to_di(s.value);
+    s = document.getElementById("translated");
+    s.innerHTML = text;
+}
+
+function DI2BF(){
+    var s = document.getElementById("beTranslate");
+    var text = di_to_bf(s.value);
     s = document.getElementById("translated");
     s.innerHTML = text;
 }
